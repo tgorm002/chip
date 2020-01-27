32decf682743954dfcd308aed18c4a29cff42dd2
Trevor Gorman <tgorm002@ucr.edu>
2020-01-26 18:00:46 -0800

    what

--- lab5/source/main.c
+++ lab5/source/main.c
@@ -12,12 +12,98 @@
 #include "simAVRHeader.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
+enum Compute_States { Start, Init, Wait, Inc, Dec, Zero} Compute_State;
 
-    /* Insert your solution below */
-    while (1) {
+void Com()
+{
+	switch(Compute_State) {   // Transitions
+		case Start:  // Initial transition
+		Compute_State = Init;
+		unsigned temp = ~PINA&0x0F;
+		break;
 
-    }
-    return 1;
+		case Init:
+		temp = ~PINA&0x0F;
+		if (temp == 0x01) {
+			Compute_State = Inc;
+		}
+		else if (temp == 0x02) {
+			Compute_State = Dec;
+		}
+		else if (temp == 0x03) {
+			Compute_State = Zero;
+		}
+		break;
+
+		case Wait:
+		temp = ~PINA&0x0F;
+		if ((temp == 0x01)||(temp == 0x02)){
+			Compute_State = Wait;
+		}
+		else if((temp == 0x01)&&(temp == 0x02)){
+			Compute_State = Zero;
+		}
+		else{
+			Compute_State = Init;
+		}
+		break;
+		
+		case Inc:
+		Compute_State = Wait;
+		break;
+		
+		case Dec:
+		Compute_State = Wait;
+		break;
+		
+		case Zero:
+		Compute_State = Wait;
+		break;
+
+		default:
+		Compute_State = Start;
+		break;
+	} // Transitions
+
+	switch(Compute_State) {   // State actions
+		case Init:
+		break;
+		
+		case Wait:
+		break;
+		
+		case Inc:
+		if (PORTB < 9) {
+			PORTB = PORTB + 1;
+		}
+		break;
+
+		case Dec:
+		if (PORTB > 0) {
+			PORTB = PORTB - 1;
+		}
+		break;
+		
+		case Zero:
+		PORTB = 0;
+		break;
+
+		default:
+		break;
+	} // State actions
 }
+
+int main() {
+	// Initialize outputs
+	Compute_State = Start; // Indicates initial call
+	DDRA = 0x00; PORTA = 0xFF;
+	DDRB = 0xFF; PORTB = 0x00;
+	PORTB = 0x00;
+	while(1) {
+		
+		Com();
+	}
+}
+
+
+
